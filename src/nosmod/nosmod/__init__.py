b4b3420d8ef89506cfe72c74d9dab0339fc555ad
Jan Duchscherer <duchsche@hm.edu>
2024-07-10 10:22:13 +0200

    chore: clean-up and rename

--- src/nosmod/nosmod/__init__.py
+++ src/nosmod/nosmod/__init__.py
@@ -1,6 +1,6 @@
 from .config import Config, HyperParameters, SqrtFilterParams
 from .lit_trainer_factory import LitTrainerFactory
-from .sqrt_filter.lit_datamodule import SqrtFilterDataModule, SqrtFilterDataset
+from .sqrt_filter.filter_dataset import SqrtFilterDataModule, SqrtFilterDataset
 from .sqrt_filter.lit_module import LitSqrtFilterModule
 
 __all__ = [
--- src/nosmod/setup.py
+++ src/nosmod/setup.py
@@ -1,9 +1,9 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="dl_modulator",
+    name="nosmod",
     version="0.1.0",
-    description="A DL based modulator for optical communication to allow ultra narrow filtering",
+    description="A DL based modulator & demodulator for optical communication to allow ultra narrow filtering.",
     author="Jan Duchscherer",
     author_email="jan.duchscherer@hm.edu",
     url="https://github.com/yourusername/dl-modulator",
