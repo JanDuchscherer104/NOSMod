3baa822f68944e96bb0be622997401f5deef0985
Jan Duchscherer <duchsche@hm.edu>
2024-07-31 09:30:15 +0200

    feat: Refactor ModulatorParams and add PSKModulator and AMModulator classes
    - Refactor the ModulatorParams class to include a default value for the target attribute.
    - Add the PSKModulator and AMModulator classes for Phase Shift Keying and Amplitude Modulation.
    - Update the ModulationType enum to handle different modulation types.
    - Include a setup_target method in the ModulationType enum.
    - Remove commented out code for ArbitraryModulator class.

--- src/nosmod/nosmod/network_components/modulator.py
+++ src/nosmod/nosmod/network_components/modulator.py
@@ -1,14 +1,15 @@
 from enum import Enum, auto
-from typing import Optional, Union
+from typing import Optional, Type, Union
 
 import torch
+from pydantic import Field
 from torch import Tensor, nn
 
 from ..utils import BaseConfig
 
 
 class ModulatorParams(BaseConfig[Union["PSKModulator", "AMModulator"]]):
-    target: "ModulationType"
+    target: "ModulationType" = Field(default_factory=lambda: ModulationType.PSK)
     num_constellation_points: Optional[int] = 4
 
 
@@ -25,23 +26,6 @@ class PSKModulator(nn.Module):
             1j * 2 * torch.pi * x_y / self.params.num_constellation_points
         )
         return torch.stack((x_x_mod, x_y_mod), dim=-1)
-
-
-# class ArbitraryModulator(nn.Module):
-#     def __init__(self, constpoints: Tensor):
-#         super().__init__()
-#         self.constpoints = constpoints
-
-#     def forward(self, x_x: Tensor, x_y: Tensor) -> Tensor:
-#         sps = self.constpoints.shape[-1]
-
-#         indices_x = x_x.long().unsqueeze(-1).expand(-1, sps)
-#         indices_y = x_y.long().unsqueeze(-1).expand(-1, sps)
-
-#         x_x_mod = self.constpoints[0, 0, indices_x]
-#         x_y_mod = self.constpoints[1, 0, indices_y]
-
-#         return torch.stack((x_x_mod, x_y_mod), dim=-1).to(torch.complex64)
 
 
 class AMModulator(nn.Module):
@@ -70,10 +54,5 @@ class ModulationType(Enum):
                 return PSKModulator(params)
             case cls.AM:
                 return AMModulator(params)
-            # case cls.ARBITRARY:
-            #     assert (
-            #         constpoints is not None
-            #     ), "constpoints must be provided for ARBITRARY modulation"
-            #     return ArbitraryModulator(constpoints)
             case _:
                 raise ValueError(f"Unknown modulation type: {params.target}")
